e7db5dff857a9fcf29f2f7657a4ecc67cff993b0
RODEL123D <102315661+RODEL123D@users.noreply.github.com>
2022-03-31 18:03:34 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+var questions =[
+    {
+num : 1,
+question : "Inside which HTML element do we put the JavaScript?",
+answer : "script",
+options : [
+"javascript",
+"js",
+"scripting",
+"script"
+        ]
+    },
+    {
+num: 2,
+question: "What is the correct syntax for referring to an external script called (xxx.js)?",
+answer: "script src=(xxx.js)",
+options: [
+"script href=(xxx.js)",
+"script src=(xxx.js)",
+"script name=(xxx.js)",
+"None of the above"
+]
+},
+{
+num: 3,
+question: "How do you create a function in JavaScript?",
+answer: "function myFunction()",
+options: [
+"function myFunction()",
+"function:myFunction()",
+"function = myFunction()",
+"None of the above"
+]
+},
+
+{
+num: 4,
+question: "How to write an IF statement in JavaScript?",
+answer: "if (i == 5)",
+options: [
+"if i = 5",
+"if i == 5 then",
+"if (i == 5)",
+"if i = 5 then"
+]
+},
+{
+num: 5,
+question: "How can you add a comment in a JavaScript?",
+answer: "//This is a comment",
+options: [
+"!--This is a comment--",
+"//This is a comment",
+"'This is a comment",
+"None of the above"
+]
+},
+
+
+
+
+];
